0dae058367a6486b2b4d9933a2633c124efa5e4d
hasanisu <hasanjimam@gmail.com>
2023-09-04 16:45:02 +0900

    try to make wishlist api

--- index.js
+++ index.js
@@ -14,14 +14,16 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dkggbgt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {serverApi: {version: ServerApiVersion.v1,strict: true,
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1, strict: true,
     deprecationErrors: true,
   }
 });
 
 async function run() {
   try {
-    
+
     //declare collection
 
 
@@ -30,142 +32,172 @@ async function run() {
     const toyotaCollection = client.db("usedCar").collection("toyota")
     const hondaCollection = client.db("usedCar").collection("honda")
     const carsCollection = client.db("usedCar").collection("cars")
-    const reconditionCategory = client.db("usedCar").collection("category") 
+    const reconditionCategory = client.db("usedCar").collection("category")
+    const cartsCollection = client.db("usedCar").collection("carts")
 
 
 
-/*  All Route Start From Here */
+    /*  All Route Start From Here */
 
-//get user by email
-app.get('/user/:email', async(req, res)=>{
-  const email = req.params.email;
-  const query = {email: email}
-  const result = await userCollection.findOne(query)
-  res.send(result)
+    //get user by email
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const result = await userCollection.findOne(query)
+      res.send(result)
 
-})
+    })
 
     //Save user to DB
     app.put('/user/:email', async (req, res) => {
-    const email = req.params.email;
-    console.log(email)
-    const user = req.body;
-    const filter = { email: email }
-    const options = { upsert: true }
-    
-    const docUpdate = {
-      $set: user
-    }
-    const result = await userCollection.updateOne(filter, docUpdate, options)
+      const email = req.params.email;
+      console.log(email)
+      const user = req.body;
+      const filter = { email: email }
+      const options = { upsert: true }
 
-    // const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
-    //   expiresIn: '1d'
+      const docUpdate = {
+        $set: user
+      }
+      const result = await userCollection.updateOne(filter, docUpdate, options)
+
+      // const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+      //   expiresIn: '1d'
+      // })
+      // res.send({ result, token })
+
+
+      res.send(result)
+    })
+
+    //ALL CARS GET, POST, PUT, DELETE START FROM HERE
+
+    app.get('/usedCarCategory', async (req, res) => {
+      const query = {}
+      const result = await reconditionCategory.find(query).toArray();
+      res.send(result)
+    })
+
+
+    app.get('/categories', async (req, res) => {
+      const query = {}
+      const result = await categoryCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+
+    //Get All cars by id 
+    app.get('/all-car/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await carsCollection.findOne(query)
+      res.send(result)
+    })
+
+
+
+    //get All toyota cars
+    app.get('/all-cars/toyota', async (req, res) => {
+      // const maker = req.query.maker;
+      // const car = req.body;
+      const query = { maker: { $in: ['toyota', 'Toyota', 'TOYOTA'] } }
+      const cursor = carsCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    //get All Honda cars
+    app.get('/all-cars/honda', async (req, res) => {
+      const query = { maker: { $in: ['honda', 'Honda', 'HONDA'] } }
+      const cursor = carsCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    //Get All New arrival 2023 cars 
+    app.get('/new-arrivals', async (req, res) => {
+      const query = { productionYear: { $eq: 2023 } }
+      const cursor = carsCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+    //Get All Sale 2023 cars
+    app.get('/car-sale', async (req, res) => {
+      const query = { sale: { $gt: 0 } }
+      const cursor = carsCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+    //get All recondition category cars
+
+    app.get('/category/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { brand_id: { $eq: id } }
+      const result = await carsCollection.find(filter).toArray()
+      res.send(result)
+    })
+
+
+    app.post('/all-car', async (req, res) => {
+      const car = req.body;
+      const result = await carsCollection.insertOne(car)
+      console.log(result)
+      res.send(result)
+
+    })
+
+    //get data by Seller email 
+    app.get('/post-cars', async (req, res) => {
+      const email = req.query.email;
+      const query = { 'seller.email': email }
+      const result = await carsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
+
+    // get user carts list 
+    app.get('/carts')
+
+
+
+    //Add user carts list
+    app.put('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const item = req.body;
+      const filter = { _id: id }
+      const options = { upsert: true }
+      const docUpdate = {
+        $set: item
+      }
+      const result = await cartsCollection.insertOne(filter,docUpdate, options )
+      res.send(result)
+    })
+
+    // delete user carts list
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log('checked', id)
+      const query = { _id: new ObjectId(id) }
+      console.log(query)
+      const result = await cartsCollection.deleteOne(query);
+      console.log(result)
+      res.send(result)
+    })
+
+
+
+
+    // HOST INFO START FROM HERE
+    // app.post('/host-request', async(req, res) =>{
+    //   const sellerRequest = req.body; 
+    //   const 
     // })
-    // res.send({ result, token })
-
-  
-    res.send(result)
-  })
-    
-//ALL CARS GET, POST, PUT, DELETE START FROM HERE
-
-  app.get('/usedCarCategory', async(req, res)=>{
-    const query = {}
-    const result = await reconditionCategory.find(query).toArray();
-    res.send(result)
-  })
-
-
-  app.get('/categories', async(req, res)=>{
-    const query = {}
-    const result = await categoryCollection.find(query).toArray();
-    res.send(result)
-  })
-
-
-
-  //Get All cars by id 
-  app.get('/all-car/:id', async(req, res) =>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await carsCollection.findOne(query)
-    res.send(result)
-  })
-
-
-
-  //get All toyota cars
-  app.get('/all-cars/toyota', async(req, res) =>{
-    // const maker = req.query.maker;
-    // const car = req.body;
-    const query = { maker :{$in : ['toyota', 'Toyota', 'TOYOTA'] }}
-    const cursor = carsCollection.find(query)
-    const result = await cursor.toArray()
-    res.send(result)
-  })
-
-  //get All Honda cars
-  app.get('/all-cars/honda', async(req, res) =>{
-    const query = { maker :{$in : ['honda', 'Honda', 'HONDA'] }}
-    const cursor = carsCollection.find(query) 
-    const result = await cursor.toArray()
-    res.send(result)
-  })
-
-  //Get All New arrival 2023 cars 
-  app.get('/new-arrivals', async(req, res) =>{
-    const query = { productionYear :{$eq : 2023 }}
-    const cursor = carsCollection.find(query) 
-    const result = await cursor.toArray()
-    res.send(result)
-  })
-
-
-   //Get All Sale 2023 cars
-   app.get('/car-sale', async(req, res) =>{
-    const query = { sale :{$gt : 0 }}
-    const cursor = carsCollection.find(query) 
-    const result = await cursor.toArray()
-    res.send(result)
-  })
-
-
-   //get All recondition category cars
-
-   app.get('/category/:id', async (req, res) =>{
-    const id = req.params.id;
-    const filter = {brand_id:{$eq: id}}
-    const result = await carsCollection.find(filter).toArray()
-    res.send(result)
-   })
-
-
-   app.post('/all-car', async(req, res) =>{
-    const car = req.body;
-    const result = await carsCollection.insertOne(car)
-    console.log(result)
-    res.send(result)
-     
-   })
-
-   //get data by Seller email 
-   app.get('/post-cars', async(req, res)=>{
-    const email = req.query.email;
-    const query = {'seller.email': email}
-    const result = await carsCollection.find(query).toArray()
-    res.send(result)
-   }) 
-
-
-
-
-
-
-  // HOST INFO START FROM HERE
-  // app.post('/host-request', async(req, res) =>{
-  //   const sellerRequest = req.body; 
-  //   const 
-  // })
 
 
 
@@ -174,7 +206,7 @@ app.get('/user/:email', async(req, res)=>{
 
     console.log("You successfully connected to MongoDB!");
 
-  } 
+  }
   finally {
     // await client.close();
   }
